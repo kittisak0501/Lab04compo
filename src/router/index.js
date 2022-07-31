9517c08645b8718cfb73e8821260ff8babd33109
kittisak <kittisak_kae@cmu.ac.th>
2022-07-31 22:19:22 +0700

    4 add error page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
-import EventDetails from '../views/EventDetailView.vue'
+import EventDetails from '../views/event/EventDetailView.vue'
+import EventRegister from '../views/event/EventRegisterView.vue'
+import EventEdit from '../views/event/EventEditView.vue'
+import EventLayout from '../views/event/EventLayoutView.vue'
+import NotFound from '../views/NotFoundView.vue'
+import NetworkError from '../views/NetworkErrorView.vue'
 const routes = [
   {
     path: '/',
@@ -19,9 +24,45 @@ const routes = [
   },
   {
     path: '/events/:id',
-    name: 'EventDetails',
-    component: EventDetails,
+    name: 'EventLayout',
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails,
+        props: true
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister,
+        props: true
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit,
+        props: true
+      }
+    ],
     props: true
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError
   }
 ]
 
